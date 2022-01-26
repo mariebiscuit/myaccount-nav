77a1406043095a0578bc19813c072808886b1f3a
Alyssa Loo <alyssaloosg@gmail.com>
2022-01-26 13:17:29 -0500

    Removed samples

--- runner.py
+++ runner.py
@@ -6,9 +6,8 @@ if __name__ == '__main__':
         password = f.readline()
 
         with MyAccountDriver(username, password) as driver:
-            # ENTER YOUR SCRIPT AFTER HERE:
-            sample_create = Edit_Details("ABC", "This is the comment.", "Josiah Carberry")
-            # driver.exe_admin_id('FILENAME.csv', "Email", "C", sample_create)
+            #ENTER YOUR SCRIPT AFTER HERE:
+            
 
-            # sample_delete = Edit_Details("ABC", "This is the comment.", "Josiah Carberry", "josiah car", expiry_reason = "Revoked", date = "01/26/2022" )
-            # driver.exe_admin_id('FILENAME.csv', "Email", "D", sample_delete)
+
+            pass
